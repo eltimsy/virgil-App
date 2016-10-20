ae7dc0521caa2bcb4c747b04f7a4dfd884e7a42e
Timothy Tse <TimmyC@Timothys-MacBook-Air.local>
2016-10-20 17:54:04 -0400

    Add react native contacts library and can now access contact list from phone

--- index.android.js
+++ index.android.js
@@ -4,8 +4,8 @@
  * @flow
  */
  import React, { Component } from 'react';
- import { AppRegistry, Text, TextInput, Image, View, StyleSheet} from 'react-native';
-
+ import { AppRegistry, Text, TextInput, Image, View, StyleSheet, Navigator} from 'react-native';
+ import MyScene from './pages/MyScene';
 
  class Pictures extends Component {
    render() {
@@ -22,56 +22,41 @@
        blue: 'blue',
        text: ''
      };
-
-     setInterval(() => {
-       this.setState({ blue: !this.state.blue});
-     }, 1000);
    }
 
-   handleKeyPress(e) {
-     e.preventDefault();
-     if(e.key=== "Enter"){
-       this.setState({text: ""})
-     }
-   }
    render() {
      let stuff = this.state.blue ? 'blue' : 'red' ;
      let pic = {
        uri: 'http://static1.1.sqspcdn.com/static/f/157301/9552810/1290590721240/1467551-9552809-thumbnail.jpg?token=1ca5iFS9emKfwTIwI5AI%2FoJRbl4%3D'
      };
      return (
-       <View style={{flex: 1}}>
-         <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}}>
-          <View style={{width: 100, height: 100, backgroundColor: 'steelblue', borderRadius: 50}} />
-          <Pictures pics={pic} />
+      <Navigator
+       initialRoute={{ title: 'My Initial Scene', index: 0 }}
+       renderScene={(route, navigator) =>
+       <MyScene
+         title={route.title}
 
-         </View>
-         <View style={{flex: 2, flexDirection: 'column'}}>
-           <TextInput
-             style={{height:80}}
-             placeholder="type something"
-             onChangeText={(text) => this.setState({text})}
-             onKeyPress={this.handlePress.bind(this)}
-            value={this.state.text}
+         // Function to call when a new scene should be displayed
+         onForward={ () => {
+           const nextIndex = route.index + 1;
+           navigator.push({
+             title: 'Scene ' + nextIndex,
+             index: nextIndex,
+           });
+         }}
 
-           />
-           <Text style={{padding: 10, fontSize: 42}}>
-            {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-           </Text>
-
-           <Text style={{
-             color: stuff,
-             fontSize: 30,
-             backgroundColor: 'teal',
-             textAlign: 'center'
-           }}>alsdkfj</Text>
-           <Text style={styles.hello}>Hello world!</Text>
-         </View>
-         {/* <View style={{flex: 2, flexDirection: 'row'}}>
-
-         </View> */}
-       </View>
-     );
+         // Function to call to go back to the previous scene
+         onBack={() => {
+           if (route.index > 0) {
+             navigator.pop();
+           }
+          }}
+         />
+       }
+       configureScene={(route, routeStack) =>
+        Navigator.SceneConfigs.FadeAndroid}
+     />
+     )
    }
  }
 
@@ -86,51 +71,3 @@
  });
 
  AppRegistry.registerComponent('VirgilApp', () => VirgilApp);
-
-// import React, { Component } from 'react';
-// import {
-//   AppRegistry,
-//   StyleSheet,
-//   Text,
-//   View
-// } from 'react-native';
-//
-// export default class VirgilApp extends Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>
-//           Welcome to React Native!
-//         </Text>
-//         <Text style={styles.instructions}>
-//           To get started, edit index.android.js
-//         </Text>
-//         <Text style={styles.instructions}>
-//           Double tap R on your keyboard to reload,{'\n'}
-//           Shake or press menu button for dev menu
-//         </Text>
-//       </View>
-//     );
-//   }
-// }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
-//
-// AppRegistry.registerComponent('VirgilApp', () => VirgilApp);
