2891b2dadfa69f425b2cea72cd0b0f665d2d7b0a
Micah Smith <m.a.smith.nomad@gmail.com>
2016-10-21 09:38:24 -0400

    view and methods for authentication added; still needs debugging

--- index.android.js
+++ index.android.js
@@ -4,8 +4,20 @@
  * @flow
  */
  import React, { Component } from 'react';
- import { AppRegistry, Text, TextInput, Image, View, StyleSheet} from 'react-native';
+ import { Alert, AppRegistry, AsyncStorage, Text, TextInput, Image, View, StyleSheet} from 'react-native';
+ import t from 'tcomb-form-native';
 
+ const STORAGE_KEY = 'id_token';
+ const Form = t.form.Form;
+ const User = t.struct({
+   first_name: t.String,
+   last_name: t.String,
+   password: t.String,
+   email: t.String,
+   remember_me: t.Boolean
+ });
+
+ let options = {};
 
  class Pictures extends Component {
    render() {
@@ -13,6 +25,92 @@
        <Image source={this.props.pics} style={{width: 150, height: 150}}/>
      );
    }
+ }
+
+ class JWTStore extends Component {
+   constructor(props) {
+     super(props)
+   }
+
+   async onValueChange(item, value) {
+     try {
+       await AsyncStorage.setItem(item, value);
+     } catch (err) {
+       console.log(`AsyncStorage error: ${err.message}`);
+     }
+   }
+
+   async onLogAttempt() {
+     let TOKEN = await AsyncStorage.getItem(STORAGE_KEY);
+     let socket = io.connect('http://localhost:8080');
+     socket.on('connect', () => {
+       socket
+        .emit('authenticate', {id_token: TOKEN})
+        .on('authenticated', () => {
+          // logic goes here (the main app stuff)
+        })
+        .on('unauthenticated', (message) => {
+          console.log(`Could not authenticate: ${JSON.stringify(message.data)}.`);
+          throw new Error(message.data.type);
+        })
+     });
+   }
+
+   userSignup() {
+     let value = this.refs.form.getValue();
+     if (value) {
+       fetch('http://localhost:8080/users', {
+         method: 'POST',
+         headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json'
+         },
+         body: JSON.stringify({
+           first_name: value.first,
+           last_name: value.last,
+           password: value.password,
+           email: value.email,
+           remember_me: value.remember
+         })
+       })
+       .then((res) => response.json())
+       .then((res) => {
+         this.onValueChange(STORAGE_KEY, res.id_token)})
+       .done();
+     }
+   }
+
+   userLogin() {
+     let value = this.refs.form.getValue();
+     if (value) {
+       fetch('http://localhost:8080/sessions/create', {
+         method: 'POST',
+         headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json'
+         },
+         body: JSON.stringify({
+           email: value.email,
+           password: value.password
+         })
+       })
+       .then((res) => res.json())
+       .then((res) => {
+         this.onValueChange(STORAGE_KEY, res.id_token)
+       })
+       .done();
+     }
+   }
+
+   async userLogout() {
+     try {
+       await AsyncStorage.removeItem(STORAGE_KEY);
+       Alert.alert("Logout succeeded.")
+     } catch (err) {
+       console.log(`AsyncStorage error: ${err}.`)
+     }
+   }
+
  }
 
  class VirgilApp extends Component {
@@ -34,46 +132,77 @@
        this.setState({text: ""})
      }
    }
-   render() {
-     let stuff = this.state.blue ? 'blue' : 'red' ;
-     let pic = {
-       uri: 'http://static1.1.sqspcdn.com/static/f/157301/9552810/1290590721240/1467551-9552809-thumbnail.jpg?token=1ca5iFS9emKfwTIwI5AI%2FoJRbl4%3D'
-     };
-     return (
-       <View style={{flex: 1}}>
-         <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}}>
-          <View style={{width: 100, height: 100, backgroundColor: 'steelblue', borderRadius: 50}} />
-          <Pictures pics={pic} />
 
-         </View>
-         <View style={{flex: 2, flexDirection: 'column'}}>
-           <TextInput
-             style={{height:80}}
-             placeholder="type something"
-             onChangeText={(text) => this.setState({text})}
-             onKeyPress={this.handlePress.bind(this)}
-            value={this.state.text}
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.row}>
+          <Text style={styles.title}>Signup/Login</Text>
+        </View>
+        <View style={styles.row}>
+          <Form
+            ref="form"
+            type={User}
+            options={options}
+          />
+        </View>
+        <View style={styles.row}>
+          <TouchableHighlight style={styles.button} onPress={this._userSignup} underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Signup</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.button} onPress={this._userLogin} underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableHighlight>
+        </View>
+        <View style={styles.row}>
+          <TouchableHighlight onPress={this._getProtectedQuote} style={styles.button}>
+            <Text style={styles.buttonText}>Filler Text</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
 
-           />
-           <Text style={{padding: 10, fontSize: 42}}>
-            {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-           </Text>
-
-           <Text style={{
-             color: stuff,
-             fontSize: 30,
-             backgroundColor: 'teal',
-             textAlign: 'center'
-           }}>alsdkfj</Text>
-           <Text style={styles.hello}>Hello world!</Text>
-         </View>
-         {/* <View style={{flex: 2, flexDirection: 'row'}}>
-
-         </View> */}
-       </View>
-     );
-   }
- }
+ //   render() {
+ //     let stuff = this.state.blue ? 'blue' : 'red' ;
+ //     let pic = {
+ //       uri: 'http://static1.1.sqspcdn.com/static/f/157301/9552810/1290590721240/1467551-9552809-thumbnail.jpg?token=1ca5iFS9emKfwTIwI5AI%2FoJRbl4%3D'
+ //     };
+ //     return (
+ //       <View style={{flex: 1}}>
+ //         <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}}>
+ //          <View style={{width: 100, height: 100, backgroundColor: 'steelblue', borderRadius: 50}} />
+ //          <Pictures pics={pic} />
+ //
+ //         </View>
+ //         <View style={{flex: 2, flexDirection: 'column'}}>
+ //           <TextInput
+ //             style={{height:80}}
+ //             placeholder="type something"
+ //             onChangeText={(text) => this.setState({text})}
+ //             /* onKeyPress={this.handlePress.bind(this)} */
+ //            value={this.state.text}
+ //
+ //           />
+ //           <Text style={{padding: 10, fontSize: 42}}>
+ //            {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+ //           </Text>
+ //
+ //           <Text style={{
+ //             color: stuff,
+ //             fontSize: 30,
+ //             backgroundColor: 'teal',
+ //             textAlign: 'center'
+ //           }}>alsdkfj</Text>
+ //           <Text style={styles.hello}>Hello world!</Text>
+ //         </View>
+ //         {/* <View style={{flex: 2, flexDirection: 'row'}}>
+ //
+ //         </View> */}
+ //       </View>
+ //     );
+ //   }
+ // }
 
  const styles = StyleSheet.create({
    hello: {
