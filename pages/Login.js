6d3b46a98f8d52ccde3642c515eccab81b66c710
Timothy Tse <timothy.sc.tse@gmail.com>
2016-10-27 16:45:55 -0400

    css changes to login and sign up page

--- pages/Login.js
+++ pages/Login.js
@@ -4,7 +4,6 @@ import React, { Component } from 'react';
 import { ScrollView, StyleSheet, View, Text, Navigator, TouchableHighlight, TouchableOpacity } from 'react-native';
 import t from 'tcomb-form-native';
 
-
 const Form = t.form.Form;
 const LoginForm = t.struct({
   email: t.String,
@@ -37,7 +36,7 @@ class LoginPage extends Component {
 
   render() {
     return (
-      <ScrollView>
+      <ScrollView style={{backgroundColor: '#A3C4BC'}}>
         <View style={styles.container}>
           <View style={styles.row}>
             <Text style={styles.title}>Login</Text>
@@ -50,7 +49,7 @@ class LoginPage extends Component {
             />
           </View>
           <View style={styles.row}>
-            <TouchableHighlight style={styles.button} onPress={this.handleLogin.bind(this)} underlayColor='#99d9f4'>
+            <TouchableHighlight style={styles.button2} onPress={this.handleLogin.bind(this)} underlayColor='#99d9f4'>
               <Text style={styles.buttonText}>Login</Text>
             </TouchableHighlight>
           </View>
@@ -66,29 +65,46 @@ class LoginPage extends Component {
 }
 
 const styles = StyleSheet.create({
+  row: {
+    backgroundColor: '#A3C4BC'
+  },
   container: {
     justifyContent: 'center',
     marginTop: 50,
     padding: 20,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#A3C4BC',
   },
   title: {
     fontSize: 30,
     alignSelf: 'center',
-    marginBottom: 30,
+    marginTop: 50,
+    marginBottom: 50,
+    color: 'white',
+    padding: 5,
+    borderRadius: 5,
   },
   buttonText: {
     fontSize: 18,
     color: 'white',
     alignSelf: 'center',
   },
-  button: {
+  button2: {
     height: 36,
-    backgroundColor: '#48BBEC',
-    borderColor: '#48BBEC',
+    backgroundColor: '#395B50',
+    borderColor: '#395B50',
     borderWidth: 1,
     borderRadius: 8,
-    marginBottom: 10,
+    marginTop: 5,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#395B50',
+    borderColor: '#395B50',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginTop: 10,
     alignSelf: 'stretch',
     justifyContent: 'center',
   },
--- pages/Signup.js
+++ pages/Signup.js
@@ -39,6 +39,8 @@ UserForm.getValidationErrorMessage = (value) => {
 }
 
 const options = {
+  auto: 'placeholders',
+  underlineColorAndroid: 'transparent',
   fields: {
     email: {
       error: 'Invalid email',
@@ -69,7 +71,7 @@ class SignupPage extends Component {
 
   render() {
     return (
-      <ScrollView>
+      <ScrollView style={{backgroundColor: '#A3C4BC'}}>
         <View style={styles.container}>
           <View style={styles.row}>
             <Text style={styles.title}>Create New Account:</Text>
@@ -98,12 +100,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginTop: 50,
     padding: 20,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#A3C4BC',
+  },
+  row: {
+    backgroundColor: '#A3C4BC',
   },
   title: {
     fontSize: 30,
     alignSelf: 'center',
     marginBottom: 30,
+    color: 'white',
   },
   buttonText: {
     fontSize: 18,
@@ -112,11 +118,12 @@ const styles = StyleSheet.create({
   },
   button: {
     height: 36,
-    backgroundColor: '#48BBEC',
-    borderColor: '#48BBEC',
+    backgroundColor: '#395B50',
+    borderColor: '#395B50',
     borderWidth: 1,
     borderRadius: 8,
     marginBottom: 10,
+    marginTop: 5,
     alignSelf: 'stretch',
     justifyContent: 'center',
   },
